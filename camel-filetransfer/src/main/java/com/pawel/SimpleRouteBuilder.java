3a944e332e1b9e2f9088055050d2910f9396aa69
theRemoteDeveloper <Pawel@192.168.1.4>
2018-12-12 12:58:56 +1100

    Added loging

--- camel-filetransfer/src/main/java/com/pawel/SimpleRouteBuilder.java
+++ camel-filetransfer/src/main/java/com/pawel/SimpleRouteBuilder.java
@@ -1,12 +1,15 @@
 package com.pawel;
 
+import org.apache.camel.LoggingLevel;
 import org.apache.camel.builder.RouteBuilder;
 
 public class SimpleRouteBuilder extends RouteBuilder {
 
 	@Override
 	public void configure() throws Exception {
-		from("file:/Users/Pawel/Desktop/inputFolder").to("file:/Users/Pawel/Desktop/outputFolder");
+		from("file:/Users/Pawel/Desktop/inputFolder")
+		.log(LoggingLevel.INFO, "Transfering file: ${file:name}")
+		.to("file:/Users/Pawel/Desktop/outputFolder");
 	}
 
 }
